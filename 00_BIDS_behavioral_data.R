561e2828195f3c777d74a3f99a896ec7ce87d9a1
Alexander Christensen <32655669+AlexChristensen@users.noreply.github.com>
2019-10-01 09:27:00 -0400

    added missing conditions code
    o added code to resolve error when conditions were missing (e.g., 1036)

--- 00_BIDS_behavioral_data.R
+++ 00_BIDS_behavioral_data.R
@@ -5,52 +5,58 @@
 library(here)
 
 # load source functions
-source("scr/org_cond_phase.R")
-source("scr/org_gen_phase.R")
-source("scr/isolate_run.R")
-source("scr/convert_to_bids.R")
+source("./scr/org_cond_phase.R")
+source("./scr/org_gen_phase.R")
+source("./scr/isolate_run.R")
+source("./scr/convert_to_bids.R")
 
 # set hard-coded variables
-path_to_data <- "~/Box/SocialAL/task/data/"
+path_to_data <- "./task/data/"
 
 # read data in
 files <- list.files(path_to_data, pattern = ".csv")
-files <- files[- (grep("button", files))]
+#files <- files[- (grep("button", files))]
 
 for (f in files) {
   dt <- read.csv(paste0(path_to_data, f))
-
+  
   # isolate conditioning runs and convert to bids
   c_run_1 <- isolate_run(dt, "cond", 1)
   b_c_run_1 <- convert_to_bids(c_run_1, "cond"); rm(c_run_1)
   c_run_2 <- isolate_run(dt, "cond", 2)
   b_c_run_2 <- convert_to_bids(c_run_2, "cond"); rm(c_run_2)
-
+  
   # isolate generalization trials and remove empty rows
-  g_run_1 <- isolate_run(dt, "gen", 1)
-  b_g_run_1 <- convert_to_bids(g_run_1, "gen"); rm(g_run_1)
-  g_run_2 <- isolate_run(dt, "gen", 2)
-  b_g_run_2 <- convert_to_bids(g_run_2, "gen"); rm(g_run_2)
-  g_run_3 <- isolate_run(dt, "gen", 3)
-  b_g_run_3 <- convert_to_bids(g_run_3, "gen"); rm(g_run_3)
-  g_run_4 <- isolate_run(dt, "gen", 4)
-  b_g_run_4 <- convert_to_bids(g_run_4, "gen"); rm(g_run_4)
-
+  
+  # removes tasks that were not completed (e.g., 1036)
+  miss <- function(...){tryCatch(..., warn=FALSE, error=function(e) return(NA))}
+  
+  g_run_1 <- miss(isolate_run(dt, "gen", 1))
+  b_g_run_1 <- miss(convert_to_bids(g_run_1, "gen")); rm(g_run_1)
+  g_run_2 <- miss(isolate_run(dt, "gen", 2))
+  b_g_run_2 <- miss(convert_to_bids(g_run_2, "gen")); rm(g_run_2)
+  g_run_3 <- miss(isolate_run(dt, "gen", 3))
+  b_g_run_3 <-miss(convert_to_bids(g_run_3, "gen")); rm(g_run_3)
+  g_run_4 <- miss(isolate_run(dt, "gen", 4))
+  b_g_run_4 <- miss(convert_to_bids(g_run_4, "gen")); rm(g_run_4)
+  
   # save with BIDS filenames
   name <- dt$participant[1]
-  dir.create(here("data", "BIDS", name))
+  #dir.create(here("data", "BIDS", name))
   dfnames <- list(b_c_run_1, b_c_run_2, b_g_run_1, b_g_run_2,
                   b_g_run_3, b_g_run_4)
-
+  
+  dfnames <- dfnames[which(!is.na(dfnames))]
+  
   for (x in 1:2) {
-    fname <- here("data", "BIDS", name,
+    fname <- here("ScanningData", "BIDS", paste("sub-",name,sep=""), "func",
                   paste0("sub-", name, "_task-cond_run-0", x, "_events.tsv"))
     vname <- dfnames[x]
     write.table(vname, fname, sep = "\t", row.names = FALSE)
   }
-
-  for (x in 1:4) {
-    fname <- here("data", "BIDS", name,
+  
+  for (x in 1:(length(dfnames)-2)) {
+    fname <- here("ScanningData", "BIDS", paste("sub-",name,sep=""), "func",
                   paste0("sub-", name, "_task-gen_run-0", x, "_events.tsv"))
     vname <- dfnames[x + 2]
     write.table(vname, fname, sep = "\t", row.names = FALSE)
