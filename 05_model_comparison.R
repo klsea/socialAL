41451299ba41550df5c406d854025c52bfc71a5d
Seaman <kls190004@cvltechs-iMac-3.local>
2020-02-26 11:30:11 -0600

    Update model comparison script

--- 05_model_comparison.R
+++ 05_model_comparison.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(ggplot2)
 
 # load source functions
+source(here::here('scr', 'concat_clean.R'))
 source(here::here('scr', 'AIC_functions.R'))
 
 # set hard-coded variables
@@ -16,12 +17,14 @@ d1 <- read.csv(here::here('output', 'two_alpha_model_params.csv' ))
 d2 <- read.csv(here::here('output', 'single_alpha_model_params.csv' ))
 d3 <- read.csv(here::here('output', 'baseline_model_params.csv'))
 
+d1 <- clean_param(d1)
+
 #merge data frames with each other - update once all subjects run with initial model
-d4 <- merge(d1[c(1,5)], d2[c(1,4)], by = 'id')
-colnames(d4) <- c('id', 'llh_double', 'llh_single')
+d4 <- merge(d1[c(1:2,6)], d2[c(1,4)], by = 'id')
+colnames(d4) <- c('id', 'agegrp', 'llh_double', 'llh_single')
 
 dt <- merge(d4, d3[c(1,3)], by = 'id')
-colnames(dt) <- c('id', 'llh_double', 'llh_single', 'llh_baseline')
+colnames(dt) <- c('id', 'agegrp', 'llh_double', 'llh_single', 'llh_baseline')
 rm(d1,d2,d3,d4)
 
 # caluclate AIC for each model
@@ -30,5 +33,8 @@ dt$AIC_single <- calc_AIC(45, 2, dt$llh_single)
 dt$AIC_baseline <- calc_AIC(45, 1, dt$llh_baseline)
 
 # calculate Weight AIC for each model and choose the winning weight
-y <- winningWeight(dt[1], dt[5:7])
+y <- winningWeight(dt$id, dt[grep('AIC_double', colnames(dt)):grep('AIC_baseline', colnames(dt))])
 d5 <- merge(dt, y, by = 'id')
+
+# proportions
+table(d5$agegrp, d5$win)
