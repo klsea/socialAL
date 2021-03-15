9cfb2a75e7df926c9e78a4a52c82f3811e7f98ee
Seaman <kls190004@cvltechs-imac-3.lan>
2021-03-15 11:45:53 -0500

    Add prior model to group comparison of model parameters

--- 13_model_parameters.R
+++ 13_model_parameters.R
@@ -1,5 +1,5 @@
 # Group differences in estimates of model parameters
-# 3.2.20
+# 3.2.20 updated 3.15.21 KLS
 
 # load required packages
 library(here)
@@ -18,21 +18,25 @@ d2 <- read.csv(here::here('output', 'single_alpha_model_params.csv'))
 d3 <- read.csv(here::here('output', 'baseline_model_params.csv'))
 o4 <- read.csv(here::here('output', 'two_alpha_with_decay_model_params_older.csv'))
 y4 <- read.csv(here::here('output', 'two_alpha_with_decay_model_params_younger.csv'))
+o5 <- read.csv(here::here('output', 'prior_model_params_older.csv'))
+y5 <- read.csv(here::here('output', 'prior_model_params_younger.csv'))
+
 d4 <- rbind(o4, y4); rm(o4, y4)
-#d5 will be priors
-d6 <- read.csv(here::here('output', 'model_comparison.csv'))[c(1,15)]
+d5 <- rbind(o5, y5); rm(o5, y5)
+d6 <- read.csv(here::here('output', 'model_comparison.csv'))[c(1,18)]
 
 # create age group labels
 dt <- clean_param(dt)
 d2 <- clean_single_alpha(d2)
 d3 <- clean_single_alpha(d3)
 d4 <- clean_single_alpha(d4)
+d5 <- clean_single_alpha(d5)
 
 # 2 alpha model ####
 # uncommenting the lines below allows analysis of 
 # only participants best-fit by the double alpha model
-dt <- merge(dt, d6, by='id')
-dt <- dt[which(dt$win == 'double'),] 
+#dt <- merge(dt, d6, by='id')
+#dt <- dt[which(dt$win == 'double'),] 
 
 # compare parameters of 2 alpha model
 older <- dt[which(dt$agegrp=='Older'),]
@@ -84,3 +88,21 @@ decaygain = t.test(older$alpha_gain, younger$alpha_gain)
 decayloss = t.test(older$alpha_loss, younger$alpha_loss)
 decaybeta = t.test(older$beta, younger$beta)
 decaydecay = t.test(older$decay, younger$decay)
+
+# 2 alpha with priors model ####
+
+# uncommenting the lines below allows analysis of 
+# only participants best-fit by the double alpha model
+#d5 <- merge(d5, d6, by='id')
+#d5 <- d4[which(d5$win == 'prior'),] 
+
+# compare parameters of 2 alpha model
+older <- d5[which(d5$agegrp=='Older'),]
+younger <- d5[which(d5$agegrp=='Younger'),]
+
+priorgain = t.test(older$alpha_gain, younger$alpha_gain)
+priorloss = t.test(older$alpha_loss, younger$alpha_loss)
+priorbeta = t.test(older$beta, younger$beta)
+priortrust = t.test(older$iProb_trust, younger$iProb_trust)
+priorneutral = t.test(older$iProb_neut, younger$iProb_neut)
+prioruntrust = t.test(older$iProb_untrust, younger$iProb_untrust)
