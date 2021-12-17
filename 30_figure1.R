369c1309aaeae6eabd0ea8243c6bd0fd186e840e
Seaman <kls190004@cvltechs-mbp.lan>
2021-12-17 10:39:51 -0600

    Update fig

--- 30_figure1.R
+++ 30_figure1.R
@@ -49,32 +49,5 @@ fig1
 # save
 save_plot(here::here('figs', 'fig1.pdf'), fig1, base_height = 7, base_width = 7)
 
-# make figure
-# multiplot(p1 + theme(legend.position = 'None'), 
-#           p3, 
-#           p2 + theme(legend.position = 'None'), 
-#           p4,
-#           cols=2)
-
-# # cowplot v1
-# ggdraw() + 
-#   draw_plot(p1 + theme(legend.position = 'none'), x = 0, y = 0.5, width = .5, height = .5) + 
-#   draw_plot(p2 + theme(legend.position = 'none'), x = 0.5, y = 0.5, width = .5, height = .5) +
-#   draw_plot(p3, x = 0, y = 0, width = .75, height = .5) + 
-#   draw_plot(p4 + theme(legend.position = 'none'), x = 0.75, y = 0, width = .25, height = .5) + 
-#   draw_plot_label(label = c("A", "B", "C", "D"), size = 15, x = c(0, 0.5, 0, 0.75), y = c(1, 1, 0.5, 0.5))
-# 
-# # cowplot v2
-# legend <- get_legend(p1 + theme(legend.position = 'top'))
-# prow <- plot_grid(
-#   p1 + theme(legend.position = 'none'),
-#   p2 + theme(legend.position = 'none'),
-#   p4 + theme(legend.position = 'none'),
-#   align = 'vh',
-#   labels = c("A", "B", "C"),
-#   hjust = -1,
-#   nrow = 1
-# )
-# plot_grid(legend, prow, p3, ncol = 1, rel_heights = c(.1, 1, 1), labels = c('', '', 'D'))
 
 
--- 31_figure2.R
+++ 31_figure2.R
@@ -10,7 +10,9 @@ library(rsvg)
 library(magick)
 
 # load source functions
-
+addline_format <- function(x,...){
+  gsub('\\s','\n',x)
+}
 # set hard-coded variables
 
 # load data
@@ -19,11 +21,11 @@ p3 <- readRDS(here::here('figs', 'tpj.RDS'))
 p5 <- readRDS(here::here('figs', 'vis.RDS'))
 
 # load images
-ifg <- image_read(here::here('figs', 'OA_vs_YA_decision_MNI152_2mm.png'))
+ifg <- image_read(here::here('figs', 'OA_vs_YA_decision_MNI152_2mm copy.png'))
 p2 <- ggdraw() + draw_image(ifg, scale = 1.1) 
-tpj <- image_read(here::here('figs', 'feedback_rpe_YAvOA_p005k50_MNI152_2mm.png'))
+tpj <- image_read(here::here('figs', 'feedback_rpe_YAvOA_p005k50 copy.png'))
 p4 <- ggdraw() + draw_image(tpj, scale = 1.1) 
-vis <- image_read(here::here('figs', 'feedback_rpe_OAvYA_p005k50_MNI152_2mm.png'))
+vis <- image_read(here::here('figs', 'feedback_rpe_OAvYA_p005k50_MNI152_2mm copy.png'))
 p6 <- ggdraw() + draw_image(vis, scale = 1.1) 
 
 # graph constants
@@ -37,8 +39,10 @@ custom_plot = list(theme(
   legend.position = 'none'))
 
 # create plot
-fig2 <- plot_grid(p2, p1 + custom_plot, p3 + custom_plot, p4, p6, p5 + custom_plot, 
-                  ncol = 2, labels = c('a', '', 'b','', 'c', ''), scale = .9)
+fig2 <- plot_grid(p2, p1 + custom_plot + scale_x_discrete(labels =addline_format(c('Younger Adults', 'Older Adults'))),
+                  p4,  p3 + custom_plot + scale_x_discrete(labels =addline_format(c('Younger Adults', 'Older Adults'))), 
+                  p6, p5 + custom_plot + scale_x_discrete(labels =addline_format(c('Younger Adults', 'Older Adults'))), 
+                  ncol = 2, labels = c('a', '', 'b','', 'c', ''), scale = .9, rel_widths = c(2, 1, 1, 2, 2, 1))
 fig2
 # save
 save_plot(here::here('figs', 'fig2.pdf'), fig2, base_height = 7, base_width = 7)
